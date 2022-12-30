56cd8d6e2cdf25a1d5d22a99a3cc2a7bf3634525
amirldn <amirmaula@outlook.com>
2022-12-30 20:40:35 +0000

    init led test

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ Code for ESP32 Devkit to display currently playing song on a bluetooth connected
 
 ### Instructions
 
-1. Follow [this](https://www.az-delivery.uk/en/blogs/azdelivery-blog-fur-arduino-und-raspberry-pi/esp32-jetzt-mit-boardverwalter-installieren) guide to set up ESP32S in Arduino IDE (AZ-Delivery ESP32S)
-2.
+1. Follow [this](https://www.az-delivery.uk/en/blogs/azdelivery-blog-fur-arduino-und-raspberry-pi/esp32-jetzt-mit-boardverwalter-installieren) guide to set up ESP32S in Arduino IDE (A)
+2. Install the [drivers](http://esp32.net/usb-uart/#:~:text=CP2102N%20Porting%20Guide-,WCH,-WinChipHead%20/%20Jiangsu%20Qinheng) for MacOS (for CH340 USB Chip)
--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,7 +1,22 @@
-void setup() {
-
+#define LEDC_CHANNEL_0 0
+#define LEDC_TIMER_13_BIT 13
+#define LEDC_BASE_FREQ 5000
+#define LED_PIN 2
+int brightness = 0;
+int fadeAmount = 5;
+void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
+     uint32_t duty = (8191 / valueMax) * min(value, valueMax); 
+     ledcWrite(channel, duty); 
 }
 
+void setup() {
+    ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
+    ledcAttachPin(LED_PIN, LEDC_CHANNEL_0);
+ }
 void loop() {
-
+    ledcAnalogWrite(LEDC_CHANNEL_0, brightness); brightness = brightness + fadeAmount;
+    if (brightness <= 0 || brightness >= 255) { 
+        fadeAmount = -fadeAmount;
+    }
+    delay(30);
 }
