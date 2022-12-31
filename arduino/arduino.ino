2958b83020ae3961b3dcea3836ffde5ae05f6b9e
amirldn <amirmaula@outlook.com>
2022-12-31 17:21:30 +0000

    drawing to display working

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,22 +1,38 @@
-#define LEDC_CHANNEL_0 0
-#define LEDC_TIMER_13_BIT 13
-#define LEDC_BASE_FREQ 5000
-#define LED_PIN 2
-int brightness = 0;
-int fadeAmount = 5;
-void ledcAnalogWrite(uint8_t channel, uint32_t value, uint32_t valueMax = 255) {
-     uint32_t duty = (8191 / valueMax) * min(value, valueMax); 
-     ledcWrite(channel, duty); 
+#include <GxEPD2_BW.h>
+#include <GxEPD2_3C.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+#include "GxEPD2_display_selection_new_style.h"
+#include "GxEPD2_display_selection.h"
+
+
+void setup()
+{
+  display.init(115200);
+  helloWorld();
+  display.hibernate();
 }
 
-void setup() {
-    ledcSetup(LEDC_CHANNEL_0, LEDC_BASE_FREQ, LEDC_TIMER_13_BIT);
-    ledcAttachPin(LED_PIN, LEDC_CHANNEL_0);
- }
-void loop() {
-    ledcAnalogWrite(LEDC_CHANNEL_0, brightness); brightness = brightness + fadeAmount;
-    if (brightness <= 0 || brightness >= 255) { 
-        fadeAmount = -fadeAmount;
-    }
-    delay(30);
+const char HelloWorld[] = "Hello World!";
+
+void helloWorld()
+{
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  int16_t tbx, tby; uint16_t tbw, tbh;
+  display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
+  // center the bounding box by transposition of the origin:
+  uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  uint16_t y = ((display.height() - tbh) / 2) - tby;
+  display.setFullWindow();
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(x, y);
+    display.print(HelloWorld);
+  }
+  while (display.nextPage());
 }
+
+void loop() {};
